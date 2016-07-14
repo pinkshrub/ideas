c8c29fef09d1ba6eabf1eb4282fae2bdd5bce6ee
Alan Weber <pinkshrub@gmail.com>
2016-07-13 18:07:03 -0700

    another things

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,11 @@ class LikesController < ApplicationController
 	before_action :req_login
 
 	def create
-		Like.create(post_id: params[:id],user_id: session[:user_id])
-		redirect_to '/bright_ideas'
+		if not Like.where(post_id: params[:id], user_id: session[:user_id]).exists?
+			Like.create(post_id: params[:id],user_id: session[:user_id])
+			redirect_to '/bright_ideas'
+		else
+			redirect_to :back
+		end
 	end
 end
